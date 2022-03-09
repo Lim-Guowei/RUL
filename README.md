186f99b7401ff2385ea48f9d5396e5e2bb3affa6
Lim Guowei <glim5.gl@gmail.com>
2022-03-09 23:33:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+### Remaining Useful Life Prediction onCommercial Modular Aero-Propulsion System Simulation (C-MAPSS) dynamical model dataset
+
+##### Data source (N-CMAPSS)
+- Link: ```https://ti.arc.nasa.gov/tech/dash/groups/pcoe/prognostic-data-repository/#turbofan-2```
+  - 17. Turbofan Engine Degradation Simulation Data Set-2
+- Description: 
+  - The generation of data-driven prognostics models requires the availability of datasets with run-to-failure trajectories. In order to contribute to the development of these methods, the dataset provides a new realistic dataset of run-to-failure trajectories for a small fleet of aircraft engines under realistic flight conditions. The damage propagation modelling used for the generation of this synthetic dataset builds on the modeling strategy from previous work . The dataset was generated with the Commercial Modular Aero-Propulsion System Simulation (C-MAPSS) dynamical model. The data set is been provided by the Prognostics CoE at NASA Ames in collaboration with ETH Zurich and PARC.
+ 
+ ##### Reference
+- Performance Benchmarking And Analysis Of Prognostic Methods For CMAPSS Datasets
+  - Link: ```https://papers.phmsociety.org/index.php/ijphm/article/view/2236```
+  - Author: Emmanuel Ramasso, Abhinav Saxena
+- Aircraft Engine Run-to-Failure Dataset under Real Flight Conditions for Prognostics and Diagnostics
+  - Link: ```https://www.researchgate.net/publication/348472709_Aircraft_Engine_Run-to-Failure_Dataset_under_Real_Flight_Conditions_for_Prognostics_and_Diagnostics```
+  - Author: Manuel Arias Chao, Chetan S Kulkarni, Kai Goebel, Olga Fink
+- PHM Society Data Challenge 2021
+  - Link: ```https://data.phmsociety.org/wp-content/uploads/sites/9/2021/08/2021_Data_Challenge.pdf```
+  - Author: Manuel Arias Chao, Chetan S Kulkarni, Kai Goebel, Olga Fink
+
---
+++ dataloader.py
@@ -0,0 +1,86 @@
+import os
+import h5py
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def Dataloader(filename):
+    dirname = "Dataset"
+    filepath = os.path.normpath(os.path.join(os.path.join(os.getcwd(), dirname), filename))
+
+    with h5py.File(filepath, "r") as hdf:
+        print("Keys: {}".format(hdf.keys()))
+        # Development set
+        W_dev = np.array(hdf.get('W_dev'))             # W  - operative condition
+        X_s_dev = np.array(hdf.get('X_s_dev'))         # X_s - measured signal
+        X_v_dev = np.array(hdf.get('X_v_dev'))         # X_v - virtual sensors
+        T_dev = np.array(hdf.get('T_dev'))             # T - engine health parameters
+        Y_dev = np.array(hdf.get('Y_dev'))             # RUL - RUL label
+        A_dev = np.array(hdf.get('A_dev'))             # Auxiliary - unit number u and the flight cycle number c, the flight class Fc and the health state h s
+
+        # Test set
+        W_test = np.array(hdf.get('W_test'))           # W
+        X_s_test = np.array(hdf.get('X_s_test'))       # X_s
+        X_v_test = np.array(hdf.get('X_v_test'))       # X_v
+        T_test = np.array(hdf.get('T_test'))           # T
+        Y_test = np.array(hdf.get('Y_test'))           # RUL  
+        A_test = np.array(hdf.get('A_test'))           # Auxiliary
+        
+        # Varnams
+        W_var = np.array(hdf.get('W_var'))
+        X_s_var = np.array(hdf.get('X_s_var'))  
+        X_v_var = np.array(hdf.get('X_v_var')) 
+        T_var = np.array(hdf.get('T_var'))
+        A_var = np.array(hdf.get('A_var'))
+
+        # from np.array to list dtype U4/U5
+        W_var = list(np.array(W_var, dtype='U20'))
+        X_s_var = list(np.array(X_s_var, dtype='U20'))  
+        X_v_var = list(np.array(X_v_var, dtype='U20')) 
+        T_var = list(np.array(T_var, dtype='U20'))
+        A_var = list(np.array(A_var, dtype='U20'))
+
+    # W = np.concatenate((W_dev, W_test), axis=0)  
+    # X_s = np.concatenate((X_s_dev, X_s_test), axis=0)
+    # X_v = np.concatenate((X_v_dev, X_v_test), axis=0)
+    # T = np.concatenate((T_dev, T_test), axis=0)
+    # Y = np.concatenate((Y_dev, Y_test), axis=0) 
+    # A = np.concatenate((A_dev, A_test), axis=0) 
+
+    dev_data = np.concatenate((W_dev, X_s_dev, X_v_dev, T_dev, A_dev, Y_dev), axis=1)
+    test_data = np.concatenate((W_test, X_s_test, X_v_test, T_test, A_test, Y_test), axis=1)
+    column_name = W_var + X_s_var + X_v_var + T_var + A_var
+    column_name.append("RUL")
+
+    print("dev_data shape: {}".format(dev_data.shape))
+    print("test_data shape: {}".format(test_data.shape))
+    print("column_name shape: {}".format(len(column_name)))
+    print("column_name: {}".format(column_name))
+    
+    # print ("W shape: " + str(W.shape))
+    # print ("X_s shape: " + str(X_s.shape))
+    # print ("X_v shape: " + str(X_v.shape))
+    # print ("T shape: " + str(T.shape))
+    # print("Y shape: " + str(Y.shape))
+    # print ("A shape: " + str(A.shape))
+    # print("Variables in W_var: {}".format(W_var))
+    # print("Variables in X_s_var: {}".format(X_s_var))
+    # print("Variables in X_v_var: {}".format(X_v_var))
+    # print("Variables in T_var: {}".format(T_var))
+    # print("Variables in A_var: {}".format(A_var))
+
+    df_dev = pd.DataFrame(data=dev_data, columns=column_name)
+    df_test = pd.DataFrame(data=test_data, columns=column_name)
+    # df_dev.iloc[100000: 100100].to_csv("df_dev_100.csv") 
+    # print(df_dev.tail(20))
+    # print(df_dev.shape)
+    # print(df_test.shape)
+
+    plt.figure()
+    plt.plot(df_dev["RUL"])
+    plt.show()
+    
+    return df_dev, df_test
+
+if __name__ == "__main__":
+    Dataloader("N-CMAPSS_DS01-005.h5")
